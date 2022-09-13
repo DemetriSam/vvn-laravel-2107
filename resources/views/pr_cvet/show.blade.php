af73a08b33df8b311e6bcc6fe12dc35ec6f2000e
Demetri Samartsev <samartsew@gmail.com>
2022-09-13 12:02:35 +0300

    add rec blocks to product card

--- resources/views/pr_cvet/show.blade.php
+++ resources/views/pr_cvet/show.blade.php
@@ -60,7 +60,7 @@
                                         </div>
                                         <div class="sharing__button sharing__button_share">
                                             <div class="sharing__pic sharing__pic_share"><img
-                                                    src="http://localhost/img/icons/share.svg" alt=""></div>
+                                                    src="http://localhost/../../img/icons/share.svg" alt=""></div>
                                             <div class="sharing__label">Поделиться</div>
 
                                         </div>
@@ -70,7 +70,7 @@
                                                 <span class="span_desktop">Копировать</span>
                                             </div>
                                             <div class="sharing__pic sharing__pic_copylink"><img
-                                                    src="http://localhost/img/icons/link.svg" alt=""></div>
+                                                    src="http://localhost/../../img/icons/link.svg" alt=""></div>
                                         </div>
                                     </div>
                                 </div>
@@ -131,4 +131,145 @@
             </div>
         </div>
     </section>
+                <section class="recomendations">
+                    <div class="rec-block">
+                        <div class="rec-block__title">
+                            <div class="rec-block__label">Еще в том же цвете</div>
+                            <div class="rec-block__decorline"><img src="../../img/icons/DecorLine.svg" alt=""></div>
+                        </div>
+                        <div class="rec-block__nodes nodes">
+                            <div class="nodes__item nodes__cell the-product" data-nid="66">
+                                <div class="product-card__item product-card__item_gallery gallery nodes__img">
+                                    <div class="swiper-grid">
+                                        <div class="swiper swiper-rec ">
+                                            <div class="swiper-wrapper">
+                                                <div class="swiper-slide"><img class="the-node-image"
+                                                        src="../../../img/recs/Shycloud-15-060820_1.jpg" alt=""></div>
+                                                <div class="swiper-slide"><img class="the-node-image"
+                                                        src="../../../img/recs/Shycloud-02-060820.jpg" alt=""></div>
+
+                                            </div>
+                                            <div class="swiper-pagination"></div>
+                                            <div class="swiper-button-prev"></div>
+                                            <div class="swiper-button-next"></div>
+
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <div class="product-card__item product-card__item_h1 h1 nodes__label">07 Shycloud</div>
+                                <div class="specs__item product-card__item product-card__item_spec price nodes__price">
+                                    €81 (м2)</div>
+                                <div class="nodes__heart"><img class="add-to-cart" src="../../img/icons/Like.svg" alt="">
+                                </div>
+                            </div>
+                            <div class="nodes__item nodes__cell">
+                                <div class="product-card__item product-card__item_gallery gallery nodes__img">
+                                    <div class="swiper-grid">
+                                        <div class="swiper swiper-rec ">
+                                            <div class="swiper-wrapper">
+                                                <div class="swiper-slide"><img src="../../../img/recs/Shycloud-02-060820.jpg"
+                                                        alt=""></div>
+                                                <div class="swiper-slide"><img src="../../../img/recs/Shycloud-15-060820_1.jpg"
+                                                        alt=""></div>
+                                                <div class="swiper-slide"><img src="../../../img/recs/Shycloud-02-060820.jpg"
+                                                        alt=""></div>
+
+                                            </div>
+                                            <div class="swiper-pagination"></div>
+                                            <div class="swiper-button-prev"></div>
+                                            <div class="swiper-button-next"></div>
+
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <div class="product-card__item product-card__item_h1 nodes__label">07 Shycloud</div>
+                                <div class="specs__item product-card__item product-card__item_spec price nodes__price">
+                                    €81 (м2)</div>
+                                <div class="nodes__heart"><img class="add-to-cart" src="../../img/icons/Like.svg" alt="">
+                                </div>
+                            </div>
+                            <div class="nodes__item nodes__cell">
+                                <div class="product-card__item product-card__item_gallery gallery nodes__img"><img
+                                        src="../../../img/recs/Shycloud-21-060820.jpg" alt=""></div>
+                                <div class="product-card__item product-card__item_h1 nodes__label">07 Shycloud</div>
+                                <div class="specs__item product-card__item product-card__item_spec price nodes__price">
+                                    €81 (м2)</div>
+                                <div class="nodes__heart"><img class="add-to-cart" src="../../img/icons/Like.svg" alt="">
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="rec-block">
+                        <div class="rec-block__title">
+                            <div class="rec-block__label">В той же коллекции</div>
+                            <div class="rec-block__decorline"><img src="../../img/icons/DecorLine.svg" alt=""></div>
+                        </div>
+                        <div class="rec-block__nodes nodes">
+                            <div class="nodes__item nodes__cell">
+                                <div class="product-card__item product-card__item_gallery gallery nodes__img"><img
+                                        src="../../../img/recs/Shycloud-rakurs-01-060820 (1).jpg" alt="">
+                                </div>
+                                <div class="product-card__item product-card__item_h1 nodes__label">07 Shycloud</div>
+                                <div class="specs__item product-card__item product-card__item_spec price nodes__price">
+                                    €81 (м2)</div>
+                                <div class="nodes__heart"><img class="add-to-cart" src="../../img/icons/Like.svg" alt="">
+                                </div>
+                            </div>
+                            <div class="nodes__item nodes__cell">
+                                <div class="product-card__item product-card__item_gallery gallery nodes__img"><img
+                                        src="../../../img/recs/Shycloud-rakurs-01-060820 (1).jpg" alt="">
+                                </div>
+                                <div class="product-card__item product-card__item_h1 nodes__label">07 Shycloud</div>
+                                <div class="specs__item product-card__item product-card__item_spec price nodes__price">
+                                    €81 (м2)</div>
+                                <div class="nodes__heart"><img class="add-to-cart" src="../../img/icons/Like.svg" alt="">
+                                </div>
+                            </div>
+                            <div class="nodes__item nodes__cell">
+                                <div class="product-card__item product-card__item_gallery gallery nodes__img"><img
+                                        src="../../../img/recs/Shycloud-rakurs-02-060820.jpg" alt="">
+                                </div>
+                                <div class="product-card__item product-card__item_h1 nodes__label">07 Shycloud</div>
+                                <div class="specs__item product-card__item product-card__item_spec price nodes__price">
+                                    €81 (м2)</div>
+                                <div class="nodes__heart"><img class="add-to-cart" src="../../img/icons/Like.svg" alt="">
+                                </div>
+                            </div>
+                            <div class="nodes__item nodes__cell">
+                                <div class="product-card__item product-card__item_gallery gallery nodes__img"><img
+                                        src="../../../img/recs/Shycloud-rakurs-15-060820_1_1.jpg" alt="">
+                                </div>
+                                <div class="product-card__item product-card__item_h1 nodes__label">07 Shycloud</div>
+                                <div class="specs__item product-card__item product-card__item_spec price nodes__price">
+                                    €81 (м2)</div>
+                                <div class="nodes__heart"><img class="add-to-cart" src="../../img/icons/Like.svg" alt="">
+                                </div>
+                            </div>
+                            <div class="nodes__item nodes__cell">
+                                <div class="product-card__item product-card__item_gallery gallery nodes__img"><img
+                                        src="../../../img/recs/Shycloud-rakurs-21-060820.jpg" alt="">
+                                </div>
+                                <div class="product-card__item product-card__item_h1 nodes__label">07 Shycloud</div>
+                                <div class="specs__item product-card__item product-card__item_spec price nodes__price">
+                                    €81 (м2)</div>
+                                <div class="nodes__heart"><img class="add-to-cart" src="../../img/icons/Like.svg" alt="">
+                                </div>
+                            </div>
+                            <div class="nodes__item nodes__cell">
+                                <div class="product-card__item product-card__item_gallery gallery nodes__img"><img
+                                        src="../../../img/recs/Shycloud-rakurs-01-060820 (1).jpg" alt="">
+                                </div>
+                                <div class="product-card__item product-card__item_h1 nodes__label">07 Shycloud</div>
+                                <div class="specs__item product-card__item product-card__item_spec price nodes__price">
+                                    €81 (м2)</div>
+                                <div class="nodes__heart"><img class="add-to-cart" src="../../img/icons/Like.svg" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
 </x-layout>
